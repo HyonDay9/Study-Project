8dfbb4983cfe6ee5a423b994c36c686abb8d2132
Oresfall <putraadhitama502@gmail.com>
2024-01-26 16:15:32 +0700

    added time

--- main.py
+++ main.py
@@ -1,1 +1,6 @@
+# Project Belajar Rana
+# Segala hal yang ada di dalam Tanda Pagar akan di Ignore oleh python / IDE (aplikasi buat jalanin kodenya)
+from time import *
+
 print ("Hello World!")
+sleep(1)
