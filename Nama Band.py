ab548d5d459822667fc9a67d67532a446cd4e460
HyonDay <157539010+HyonDay9@users.noreply.github.com>
2024-01-27 15:15:05 +0700

    Create Nama Band.py

---
+++ Nama Band.py
@@ -0,0 +1,9 @@
+#Template Code Gabungan 2 variabel-> Nama Band (FIX)
+city = input("Dimana kamu tinggal?")
+pet = input("Siapa nama hewan peliharaanmu?")
+band = city +''+ pet
+
+print(f"nama band kamu adalah: {band}")
+
+#Welp bantu ini Tutor puh sepuh
+#Uji coba = 6x (saat ini)
